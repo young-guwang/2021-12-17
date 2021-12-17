64cc371421e9e9943c28fc358a1a9495f5b4d682
young-guwang <78337433+young-guwang@users.noreply.github.com>
2021-12-17 20:16:38 +0900

    Add files via upload

---
+++ a.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class a {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] inputNum = new int[10];
+		for (int i=0; i<10; i++) {
+			inputNum[i] = sc.nextInt();
+		}
+		
+		int[] mode = new int[10];
+		
+		for (int i=0; i<10; i++) {
+			mode[inputNum[i]]++;
+		}
+		int modeNum = 0;
+		int modeCnt = 0;
+		
+		for (int i=0; i<10; i++) {
+			if (modeCnt < mode[i]) {
+				modeCnt = mode[i];
+				modeNum = i;
+			}
+		}
+		// 1 2 2 3 1 4 2 2 4 3
+		System.out.println("최빈수는:" + modeNum + " cnt:" + modeCnt); 
+	}
+	
+
+}
