e532815ec67c717c7e20f278cdb7d14f987090c9
young-guwang <78337433+young-guwang@users.noreply.github.com>
2021-12-17 20:15:17 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		String x;
+		int y;
+	
+		x = s.nextLine();
+		y = s.nextInt();
+		
+		StringTokenizer T = new StringTokenizer(x," ");
+		
+		int nh = Integer.parseInt(T.nextToken());
+		int nm = Integer.parseInt(T.nextToken());
+		int ns = Integer.parseInt(T.nextToken());
+		
+		nh = nh+(y/60/60);
+		nm = nm +(y/60);
+		ns = ns+(y%60);
+		
+		if (ns>=60) {
+			ns = ns-60;
+			nm++;}
+		
+		if(nm>=60) {
+			int wfwf = nm/60;
+			nm = nm % 60;
+			nh = nh+ wfwf;
+		}
+		
+		System.out.println(nh+" "+nm+" "+ns);
+	}
+}
+
